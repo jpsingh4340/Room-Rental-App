dcc2bfc1361f9d996d2bff70d2ac5d4b222092c5
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-16 22:37:38 +1200

    kawal- code for login page added

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Auth.css'; // Shared CSS for Login and Register
+
+function Login() {
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+      <form className="auth-form">
+        <label>Email:</label>
+        <input type="email" required />
+        <label>Password:</label>
+        <input type="password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
