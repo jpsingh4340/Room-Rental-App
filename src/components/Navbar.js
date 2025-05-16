b78b7a45f74f9664c7636b2f74eac9f318262c04
Sahilchangani40 <CHANS26@student.op.ac.nz>
2025-05-16 14:17:32 +1200

    Navigationbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // We'll create this CSS file next
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/rooms">RoomRental</Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/GuestDashboard">Dashboard</Link></li>
+        <li><Link to="/find-room">Find Room</Link></li>
+        <li><Link to="/login">Login</Link> / <Link to="/register">Register</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
