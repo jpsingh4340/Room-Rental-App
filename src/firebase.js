d6e1d9488b1c4e3bec2624c7997af3afeb4f6dd0
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-16 22:41:30 +1200

    kawal- do the changes in firebase.js code

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
 // src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: AIzaSyDkqd5N8i6GjsNz6m4R6igHXrDy-JoeddY,
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+export { auth };
+
