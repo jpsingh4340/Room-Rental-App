73ff6f9f375be8f69567647dd49f0bab086ba181
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-20 15:56:23 +1200

    kawal- remove the comments

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './Auth.css'; // Shared CSS for Login and Register
-import Login from './Login'; // Make sure path is correct
+import './Auth.css'; 
+import Login from './Login'; 
 
 const Login = () => {
   return (
