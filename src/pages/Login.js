ef4acd1b7dabf30880439773539a2aa74c7d8aeb
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-20 15:54:55 +1200

    kawal- update the code and  resolve  the code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Auth.css'; // Shared CSS for Login and Register
 
-function Login() {
+const Login = () => {
   return (
     <div className="auth-container">
       <h2>Login</h2>
       <form className="auth-form">
         <label>Email:</label>
         <input type="email" required />
+        
         <label>Password:</label>
         <input type="password" required />
+        
         <button type="submit">Login</button>
       </form>
     </div>
   );
-}
+};
 
-export default Login;
-   
+export default Login;
