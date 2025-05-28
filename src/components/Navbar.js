5ae1c203f355c24397c6edc0b6f712d3823739d2
Sahilchangani40 <CHANS26@student.op.ac.nz>
2025-05-28 22:53:32 +1200

    Sahil: done whole code for navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,13 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
+const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <nav className="navbar">
+      <Link to="/">Dashboard</Link>
+      <Link to="/findroom">Find Room</Link>
 
       {user && user.role === 'admin' && (
         <Link to="/admin/findroom">Manage Rooms</Link>
