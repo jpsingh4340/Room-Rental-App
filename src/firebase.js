d66aa0c510f48b1ee1c36d006ada06f8f9b13136
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-28 16:46:49 +1200

    kawal-add some links and id's

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+// Replace with your Firebase project config
+const firebaseConfig = {
+  apiKey: "AIzaSyDR-TcSr7qCAKCCTvCjzIE_mqvKHo67YjA",
+  authDomain: "rental-haven-c052a.firebaseapp.com",
+  projectId: "rental-haven-c052a",
+  storageBucket: "rental-haven-c052a.firebasestorage.app",
+  messagingSenderId: "167061675023",
+  appId: "1:167061675023:web:8fe3d486cca2c533ae577a"
+};
