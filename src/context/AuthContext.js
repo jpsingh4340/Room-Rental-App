a201801f3ef4c5e984331e68a1618f381dbe7207
Harpreet919 <harpreetsinghnz2003@gmail.com>
2025-05-29 13:47:08 +1200

    import In

---
+++ src/context/AuthContext.js
@@ -0,0 +1,11 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { auth, db } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+
+export const AuthContext = createContext();
