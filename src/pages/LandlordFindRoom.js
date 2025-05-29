e5e1bc72c1ab5b2c72710ce828dfd48793779a07
jpsingh4340 <jpsingh4340@gmail.com>
2025-05-30 10:15:51 +1200

    Jaspreet-coded for useeffect part

--- src/pages/LandlordFindRoom.js
+++ src/pages/LandlordFindRoom.js
@@ -4,6 +4,11 @@ import { db } from '../firebase';
 import { AuthContext } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
 
-const LandlordFindRoom = () => 
+const LandlordFindRoom = () => {
   const { user } = useContext(AuthContext);
   const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, 'rooms'), where('ownerId', '==', user.uid));
+    getDocs(q).then(snap => setRooms(snap.docs.map(d => ({ id: d.id, ...d.data() }))));
+  }, [user]);
