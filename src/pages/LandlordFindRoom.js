bce05460f2f32690d7b7afd0eba5aa39130e7fd0
jpsingh4340 <jpsingh4340@gmail.com>
2025-05-30 10:14:50 +1200

    Jaspreet-coding for authentication

--- src/pages/LandlordFindRoom.js
+++ src/pages/LandlordFindRoom.js
@@ -3,3 +3,7 @@ import { collection, getDocs, query, where, deleteDoc, doc } from 'firebase/fire
 import { db } from '../firebase';
 import { AuthContext } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
+
+const LandlordFindRoom = () => 
+  const { user } = useContext(AuthContext);
+  const [rooms, setRooms] = useState([]);
