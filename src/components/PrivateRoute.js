3d34bf829740bddfa8b10a3e0a25823dc0a1ebd2
kawal285 <kawalpreetkaur285@gmail.com>
2025-05-29 13:55:50 +1200

    kawal- finish th code

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,3 +2,12 @@
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
+const PrivateRoute = ({ children, roles }) => {
+  const { user, loading } = useContext(AuthContext);
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" />;
+  if (roles && !roles.includes(user.role)) return <Navigate to="/" />;
+  return children;
+};
+
+export default PrivateRoute;
