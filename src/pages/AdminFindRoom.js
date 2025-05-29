518bed7b6dbb5df22a4b9aaa0c02f0d62e8a0540
jpsingh4340 <jpsingh4340@gmail.com>
2025-05-30 09:59:53 +1200

    Jaspreet-imported files into it

--- src/pages/AdminFindRoom.js
+++ src/pages/AdminFindRoom.js
@@ -1,3 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase';
+import { Link } from 'react-router-dom';
+import './AdminFindRoom.css';
