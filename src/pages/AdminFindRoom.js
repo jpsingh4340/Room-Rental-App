f47bff55c1c912e721f0aaf5cee4a73ba1c16824
jpsingh4340 <jpsingh4340@gmail.com>
2025-05-30 10:00:42 +1200

    Jaspreet-coding

--- src/pages/AdminFindRoom.js
+++ src/pages/AdminFindRoom.js
@@ -3,3 +3,6 @@ import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase';
 import { Link } from 'react-router-dom';
 import './AdminFindRoom.css';
+
+const AdminFindRoom = () => {
+  const [rooms, setRooms] = useState([]);
