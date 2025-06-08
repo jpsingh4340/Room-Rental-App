979fd8163deaf6b621f978e02b96c66e9dc7a46d
jpsingh4340 <jpsingh4340@gmail.com>
2025-06-08 20:04:54 +1200

    Jaspreet- Under feedback.js file code, i have added the code for each set textboxes

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,3 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { collection, addDoc, getDocs } from "firebase/firestore";
+
+const FeedbackForm = ({ roomId }) => {
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(5);
+  const [submitted, setSubmitted] = useState(false);
