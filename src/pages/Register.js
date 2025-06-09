0e02f77338de2a7b489a544033b6c6cefde915e6
Harpreet919 <harpreetsinghnz2003@gmail.com>
2025-06-10 09:57:32 +1200

    REgister code change

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,16 +1,5 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
-
-const Register = () => {
-  const [fullName, setFullName] = useState('');
-  const [role, setRole] = useState('guest');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [terms, setTerms] = useState(false);
-  const { register } = useContext(AuthContext);
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
