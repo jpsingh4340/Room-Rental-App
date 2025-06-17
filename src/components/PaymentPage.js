a8550789961f3f8037bfd78bf3c8e38def00b1d1
kawal285 <kawalpreetkaur285@gmail.com>
2025-06-17 14:08:32 +1200

    kawal- remove the comments

--- src/components/PaymentPage.js
+++ src/components/PaymentPage.js
@@ -1,6 +1,6 @@
  import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db, auth } from '../firebase'; // ✅ Adjusted to your structure
+import { db, auth } from '../firebase'; 
 
 import './PaymentPage.css';
