c1e8d535366b6a360b16703387506de4f450b1e0
Sahilchangani40 <CHANS26@student.op.ac.nz>
2025-06-24 02:12:19 +1200

    sahil: hosting

--- src/data/sampleRooms.js
+++ src/data/sampleRooms.js
@@ -16,7 +16,7 @@ export const sampleRooms = [
       'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&w=800&q=60',
       'https://images.unsplash.com/photo-1554995207-c18c203602cb?auto=format&fit=crop&w=800&q=60'
     ],
-    ownerId: 'SOME_LANDLORD_UID'
+    ownerId: 'KJ8OZPD1erXbDv5QPWfBRcc6H113'
   },
   // 2. Chic One-Bedroom on Queen Street
   {
@@ -28,12 +28,12 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 0,
-    imageUrl: 'https://images.unsplash.com/photo-1578881337104-6a2a7e2f4709?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://c8.alamy.com/comp/KC6GM6/looking-up-through-fairy-lights-and-outdoor-lighting-at-skyscrapers-KC6GM6.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1578881337104-6a2a7e2f4709?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?auto=format&fit=crop&w=800&q=60'
+      'https://c8.alamy.com/comp/KC6GM6/looking-up-through-fairy-lights-and-outdoor-lighting-at-skyscrapers-KC6GM6.jpg',
+      'https://media.tfehotels.com/media/filer_public/87/b8/87b85346-207d-4286-980f-02b202fa9f64/adina-apartment-hotel-auckland-britomart-one-bedroom-apartment-02-2016-1500x1000.jpg'
     ],
-    ownerId: 'SOME_LANDLORD_UID'
+    ownerId: 'KJ8OZPD1erXbDv5QPWfBRcc6H113'
   },
   // 3. Luxury Penthouse with Sky Tower Views
   {
@@ -45,10 +45,10 @@ export const sampleRooms = [
     bathrooms: 2,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1560448070-920b2643a82f?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://images1.apartments.com/i2/Ri1LAU9VyY0fG6BBw1KDEiHDU1fMI2rUfL0njToIr0A/111/infiniti-105-apartments-edmonton-ab-primary-photo.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1560448070-920b2643a82f?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1533777324565-a040eb52fac2?auto=format&fit=crop&w=800&q=60'
+      'https://images1.apartments.com/i2/Ri1LAU9VyY0fG6BBw1KDEiHDU1fMI2rUfL0njToIr0A/111/infiniti-105-apartments-edmonton-ab-primary-photo.jpg',
+      'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/b0/b9/7f/adina-apartment-hotel.jpg?w=500&h=400&s=1'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -62,12 +62,12 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://images.corelogic.asia/768x512/filters:stretch()/assets/nz/perm/uhp5v5gp24i6pisaaeugmctxre?signature=fb3647fe9b3e9fb0533182d1d14c5c39aa76ec26b23741a40430be38f6ba4f69',
     imageUrls: [
-      'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1493809842364-78817add7ffb?auto=format&fit=crop&w=800&q=60'
+      'https://images.corelogic.asia/768x512/filters:stretch()/assets/nz/perm/uhp5v5gp24i6pisaaeugmctxre?signature=fb3647fe9b3e9fb0533182d1d14c5c39aa76ec26b23741a40430be38f6ba4f69',
+      'https://tse3.mm.bing.net/th?id=OIP.XFaqNQ2QhoItz7xaAgSGQQHaE8&pid=Api&P=0&h=180'
     ],
-    ownerId: 'SOME_LANDLORD_UID'
+    ownerId: 'KJ8OZPD1erXbDv5QPWfBRcc6H113'
   },
   // 5. Heritage Villa in Parnell
   {
@@ -82,7 +82,7 @@ export const sampleRooms = [
     imageUrl: 'https://images.unsplash.com/photo-1523217582562-09d0def993a6?auto=format&fit=crop&w=800&q=60',
     imageUrls: [
       'https://images.unsplash.com/photo-1523217582562-09d0def993a6?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1582719478179-4675cbf1d5ba?auto=format&fit=crop&w=800&q=60'
+      'https://www.aucklandnz.com/media/media/tau/tau_media/venues/p-4bd0f744-9efc-02e9-1d332c384d1d9b40-2544003.jpg?ext=.jpg&width=1400&height=800'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -97,10 +97,10 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 0,
-    imageUrl: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://cdn6.ep.dynamics.net/s3/rw-propertyimages/746e-H3250381-136534096__1740090993-124862-BGM9035SRA.jpg?bgcolor=595959&height=1257&mode=crop&quality=90&scale=both&width=2400&_ext=.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1493809842364-78817add7ffb?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?auto=format&fit=crop&w=800&q=60'
+      'https://cdn6.ep.dynamics.net/s3/rw-propertyimages/746e-H3250381-136534096__1740090993-124862-BGM9035SRA.jpg?bgcolor=595959&height=1257&mode=crop&quality=90&scale=both&width=2400&_ext=.jpg',
+      'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?auto=format&fit=crop&https://propertyrealestate.nz/btabcloud/uploads/2020/08/alf-place-somerville-manukau-city-house-for-sale-ray-white-botany-town-centre-1597925051lc84p.jpg=800&q=60'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -114,10 +114,10 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 0,
-    imageUrl: 'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://www.roomies.pics/image/upload/c_scale,dpr_1.0,f_jpg,fl_lossy,q_auto:good,w_672/uafectqfm3vnivaxtgvn',
     imageUrls: [
-      'https://images.unsplash.com/photo-1505761671935-60b3a7427bad?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1508154048109-0a9def03cb30?auto=format&fit=crop&w=800&q=60'
+      'https://www.roomies.pics/image/upload/c_scale,dpr_1.0,f_jpg,fl_lossy,q_auto:good,w_672/uafectqfm3vnivaxtgvn',
+      'https://gallery.streamlinevrs.com/units-gallery/00/07/83/image_159945386.jpeg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -131,9 +131,9 @@ export const sampleRooms = [
     bathrooms: 2,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=800&q=60',
-    imageUrls: ['https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?auto=format&fit=crop&w=800&q=60'
+    imageUrl: 'https://tse4.mm.bing.net/th?id=OIP.K12Tqn-6m3AMZ_NnMOraJgHaE0&pid=Api&P=0&h=180',
+    imageUrls: ['https://tse4.mm.bing.net/th?id=OIP.K12Tqn-6m3AMZ_NnMOraJgHaE0&pid=Api&P=0&h=180',
+      'https://www.theagencyre.com/blog/wp-content/uploads/2014/09/unit_2142387_studio-apartment-1024x683.jpeg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -147,10 +147,10 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/507050254.jpg?k=e54d664a5d3748ea237e008b2fc0092cfc3849ac2458c925fc7378779c933d8f&o=&hp=1',
     imageUrls: [
-      'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?auto=format&fit=crop&w=800&q=60'
+      'https://cf.bstatic.com/xdata/images/hotel/max1024x768/507050254.jpg?k=e54d664a5d3748ea237e008b2fc0092cfc3849ac2458c925fc7378779c933d8f&o=&hp=1',
+      'https://imgservice.rentbyowner.com/jpg/500x325/AzRT8xsWDLcMZ8v6cmeSExhAGkyLytc8kF2ecbmV1Pf21bBqNL3jyM1z3xYn_K8xxKkvr_f8u-aXnS9LJRxZAt8JDvGJvh5ZB0-TW5Ue3Q5ae1gqX54mDEzMgESv7L41rPtRujNczKUTaDDVYoT4jRArvrCrxTMX?property_id=HA-7312453176'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -164,9 +164,9 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 0,
     lounges: 0,
-    imageUrl: 'https://images.unsplash.com/photo-1505691723518-34ca0b51bf40?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://cdn6.ep.dynamics.net/s3/rw-propertyimages/a167-H3110728-1719198051-1-21258761751.jpg?bgcolor=595959&height=786&mode=crop&quality=90&scale=both&width=1500&_ext=.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1505691723518-34ca0b51bf40?auto=format&fit=crop&w=800&q=60',
+      'https://cdn6.ep.dynamics.net/s3/rw-propertyimages/a167-H3110728-1719198051-1-21258761751.jpg?bgcolor=595959&height=786&mode=crop&quality=90&scale=both&width=1500&_ext=.jpg',
       'https://images.unsplash.com/photo-1484154218962-a197022b5858?auto=format&fit=crop&w=800&q=60'
     ],
     ownerId: 'SOME_LANDLORD_UID'
@@ -182,9 +182,9 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1556910103-1e347b41e0f7?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://i.pinimg.com/originals/35/68/cd/3568cda443ba033507de60d9338681e4.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1556910103-1e347b41e0f7?auto=format&fit=crop&w=800&q=60',
+      'https://i.pinimg.com/originals/35/68/cd/3568cda443ba033507de60d9338681e4.jpg',
       'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?auto=format&fit=crop&w=800&q=60'
     ],
     ownerId: 'SOME_LANDLORD_UID'
@@ -199,10 +199,10 @@ export const sampleRooms = [
     bathrooms: 2,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://propertyrealestate.nz/btabcloud/uploads/2020/03/thomas-road-flat-bush-manukau-city-house-for-sale-ray-white-botany-town-centre-15851550548lp4c.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1572120360610-d971b9c5b2a8?auto=format&fit=crop&w=800&q=60'
+      'https://propertyrealestate.nz/btabcloud/uploads/2020/03/thomas-road-flat-bush-manukau-city-house-for-sale-ray-white-botany-town-centre-15851550548lp4c.jpg',
+      'https://propertyrealestate.nz/btabcloud/uploads/2021/04/west-fairway-golflands-manukau-city-house-for-sale-ray-white-botany-town-centre-1619012067c8lp4.jpg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -233,9 +233,9 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 0,
-    imageUrl: 'https://images.unsplash.com/photo-1560185127-6c9c1bc9c600?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://images1.apartments.com/i2/bj1S5LtI74BKw_qBkal08ix2U2S_pwQgXcP3n7qEU8M/111/lakewood-shores-brandon-fl-exterior-building.jpg?p=1',
     imageUrls: [
-      'https://images.unsplash.com/photo-1560185127-6c9c1bc9c600?auto=format&fit=crop&w=800&q=60',
+      'https://images1.apartments.com/i2/bj1S5LtI74BKw_qBkal08ix2U2S_pwQgXcP3n7qEU8M/111/lakewood-shores-brandon-fl-exterior-building.jpg?p=1',
       'https://images.unsplash.com/photo-1484154218962-a197022b5858?auto=format&fit=crop&w=800&q=60'
     ],
     ownerId: 'SOME_LANDLORD_UID'
@@ -251,10 +251,10 @@ export const sampleRooms = [
     bathrooms: 1,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1475170614146-0901395df6fc?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://tse1.mm.bing.net/th?id=OIF.Yra28CrhByoDbO9zbsTBgg&pid=Api&P=0&h=180',
     imageUrls: [
-      'https://images.unsplash.com/photo-1475170614146-0901395df6fc?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1455699452105-58acfae2ebd5?auto=format&fit=crop&w=800&q=60'
+      'https://tse1.mm.bing.net/th?id=OIF.Yra28CrhByoDbO9zbsTBgg&pid=Api&P=0&h=180',
+      'https://propertyrealestate.nz/btabcloud/uploads/2020/08/alf-place-somerville-manukau-city-house-for-sale-ray-white-botany-town-centre-1597925138l48pc.jpg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -270,8 +270,8 @@ export const sampleRooms = [
     lounges: 0,
     imageUrl: 'https://images.unsplash.com/photo-1455699452105-58acfae2ebd5?auto=format&fit=crop&w=800&q=60',
     imageUrls: [
-      'https://images.unsplash.com/photo-1455699452105-58acfae2ebd5?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1479514547862-2a283870c688?auto=format&fit=crop&w=800&q=60'
+      'https://www.crownmelbourne.com.au/getsydmedia/ffef15f5-bdd9-4b10-b32a-869fc0ac3dea/120422-Melbourne-Hotels-Towers-Rooms-Studio-1200x800.jpg',
+      'https://i.pinimg.com/originals/30/a7/fa/30a7fa2a3b9db3bf7a9aa05677088434.jpg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -285,10 +285,10 @@ export const sampleRooms = [
     bathrooms: 2,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1622549638678-1a7ce2fa057e?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://propertyrealestate.nz/btabcloud/uploads/2020/08/amerce-place-highland-park-manukau-city-house-for-sale-ray-white-botany-town-centre-1597924567p48lc.jpg',
     imageUrls: [
-      'https://images.unsplash.com/photo-1622549638678-1a7ce2fa057e?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1582719478179-4675cbf1d5ba?auto=format&fit=crop&w=800&q=60'
+      'https://propertyrealestate.nz/btabcloud/uploads/2020/08/amerce-place-highland-park-manukau-city-house-for-sale-ray-white-botany-town-centre-1597924567p48lc.jpg',
+      'https://propertyrealestate.nz/btabcloud/uploads/2020/06/kouras-place-flat-bush-manukau-city-house-for-sale-ray-white-botany-town-centre-1591270017lc84p.jpg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -302,10 +302,10 @@ export const sampleRooms = [
     bathrooms: 3,
     kitchens: 1,
     lounges: 1,
-    imageUrl: 'https://images.unsplash.com/photo-1572120360610-d971b9c5b2a8?auto=format&fit=crop&w=800&q=60',
+    imageUrl: 'https://tse1.mm.bing.net/th?id=OIP.WiGuw-Xvj0rReD9kt0UuPgHaE8&pid=Api&P=0&h=180',
     imageUrls: [
-      'https://images.unsplash.com/photo-1572120360610-d971b9c5b2a8?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1598928506313-f3206a839acb?auto=format&fit=crop&w=800&q=60'
+      'https://tse1.mm.bing.net/th?id=OIP.WiGuw-Xvj0rReD9kt0UuPgHaE8&pid=Api&P=0&h=180',
+      'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/b0/b9/7f/adina-apartment-hotel.jpg?w=700&h=-1&s=1'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
@@ -322,7 +322,7 @@ export const sampleRooms = [
     imageUrl: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?auto=format&fit=crop&w=800&q=60',
     imageUrls: [
       'https://images.unsplash.com/photo-1570129477492-45c003edd2be?auto=format&fit=crop&w=800&q=60',
-      'https://images.unsplash.com/photo-1615211223102-6ce799563133?auto=format&fit=crop&w=800&q=60'
+      'https://i.pinimg.com/originals/cf/a9/f8/cfa9f87173a0019b3eeeceede0684e6a.jpg'
     ],
     ownerId: 'SOME_LANDLORD_UID'
   },
